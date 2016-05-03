7222cd9d5dbfe0ef76be0a481d98cf40e380a381
Kien <kientrantrung.code@gmail.com>
2016-05-03 10:27:59 +0200

    Delete compileOrarCopyLocal.sh

--- compileOrarCopyLocal.sh
+++
@@ -1,7 +0,0 @@
-mvn package
-mv target/OrarHSHOIF-0.0.1-SNAPSHOT-jar-with-dependencies.jar target/orar.jar
-echo 'orar.jar has been created in target folder'
-echo 'Copying orar.jar to ~/benchmarks/software'
-cp target/orar.jar ~/benchmarks/software/
-#echo 'Copying log4j.properties to Frodo Server...'
-#scp target/log4j.properties kientran@frodo.informatik.uni-ulm.de:/data/kien/iswc16benchmark/software/
